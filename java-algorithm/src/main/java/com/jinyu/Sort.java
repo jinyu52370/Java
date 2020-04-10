becf5450ec2b23a0eb9f5521b0053ab3605a5107
jinyu52370 <jinyu52370@qq.com>
2020-04-10 23:52:13 +0800

    java-algorithm:Sort:优化冒泡排序；完成选择排序；完成插入排序；完成交换法的希尔排序

--- java-algorithm/src/main/java/com/jinyu/Sort.java
+++ java-algorithm/src/main/java/com/jinyu/Sort.java
@@ -6,52 +6,108 @@ package com.jinyu;
  */
 public class Sort {
     /**
-     * 直接插入排序
-     *
-     *  通过交换进行插入排序，借鉴冒泡排序
-     */
-    public static void directInsertionSort1(int[] a) {
-        for (int i = 0; i < a.length - 1; i++) {
-            for (int j = i + 1; j > 0; j--) {
-                if (a[j - 1] > a[j]) {
-                    int temp = a[j - 1];
-                    a[j - 1] = a[j];
-                    a[j] = temp;
-                }
-            }
-        }
-    }
-
-    /**
-     * 直接插入排序
-     *
-     *  通过将较大的元素都向右移动而不总是交换两个元素
-     */
-    public static void directInsertionSort2(int[] a) {
-        for (int i = 1; i < a.length; i++) {
-            int num = a[i];
-            int j;
-            for (j = i; j > 0 && num < a[j - 1]; j--) {
-                a[j] = a[j - 1];
-            }
-            a[j] = num;
-        }
-    }
-
-    /*
-     *
-     */
-
-    /**
      * 冒泡排序
      */
     public static int[] bubbleSort(int[] array){
         for (int i = 0; i < array.length - 1; i++) {
+            boolean isOrderly = true;
             for (int j = 0; j < array.length - 1 - i; j++) {
                 if (array[j] > array[j + 1]) {
+                    isOrderly = false;
                     int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
+                }
+            }
+            //若在一趟排序中均未发生交换，则表示已经有序
+            if (isOrderly){
+                break;
+            }
+        }
+        return array;
+    }
+
+    /**
+     * 选择排序
+     */
+    public static int[] selectionSort(int[] array){
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length - i; j++) {
+                if (array[minIndex] > array[j]){
+                    minIndex = j;
+                }
+            }
+            if (i != minIndex) {
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+        return array;
+    }
+
+//    /**
+//     * 直接插入排序
+//     *
+//     *  通过交换进行插入排序，借鉴冒泡排序
+//     */
+//    public static void insertionSort1(int[] a) {
+//        for (int i = 0; i < a.length - 1; i++) {
+//            for (int j = i + 1; j > 0; j--) {
+//                if (a[j - 1] > a[j]) {
+//                    int temp = a[j - 1];
+//                    a[j - 1] = a[j];
+//                    a[j] = temp;
+//                }
+//            }
+//        }
+//    }
+
+    /**
+     * 插入排序
+     *  i：无序表首元素下标
+     *  j：有序表末元素下标，在无序表首元素的前一个
+     *  value：记录无序表首元素
+     *  array[j]：有序表末元素
+     *
+     *  若无序表首元素小于有序表末元素则有序表末元素后移（占据无序表首元素位置，已记录在value不会丢失），
+     *  有序表末元素下标前移，直到待插入元素小于无序表首元素，则无序表首元素插入j的后面
+     */
+    public static int[] insertionSort(int[] array){
+        for (int i = 1; i < array.length; i++) {
+            int value = array[i];
+            int j;
+            for (j = i - 1; j >= 0 && value < array[j]; j--){
+                array[j + 1] = array[j];
+            }
+            /*
+             * 找到插入位置，在待插入元素的后一个
+             *
+             * 若value > array[i]，即无序表首元素大于有序表末元素，即j + 1 == i则无需插入
+             */
+            if (j + 1 != i) {
+                array[j + 1] = value;
+            }
+        }
+        return array;
+    }
+
+    /**
+     * 希尔排序
+     */
+    public static int[] shellSort(int[] array) {
+        int temp;
+        for (int stepLength = array.length / 2; stepLength > 0; stepLength /= 2) {
+            for (int i = stepLength; i < array.length; i++) {
+                //遍历各组中元素
+                for (int j = i - stepLength; j >= 0; j -= stepLength) {
+                    //前一个元素大于加上步长的元素则交换
+                    if (array[j] > array[j + stepLength]) {
+                        temp = array[j];
+                        array[j] = array[j + stepLength];
+                        array[j + stepLength] = temp;
+                    }
                 }
             }
         }
--- java-algorithm/src/test/java/com/jinyu/SortTest.java
+++ java-algorithm/src/test/java/com/jinyu/SortTest.java
@@ -2,13 +2,47 @@ package com.jinyu;
 
 import org.junit.Test;
 
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 /**
  * @author <a href="jinyu52370@163.com">JJJ</a>
  * @date 2020/4/4 16:47
  */
 public class SortTest {
     @Test
-    public void test(){
-        Sort.bubbleSort(new int[]{9,8,7,6,5,4,3,2,1});
+    public void data8WTest(){
+        int[] data = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            data[i] = (int) (Math.random() * 80000);
+        }
+        Timestamp t1 = new Timestamp(System.currentTimeMillis());
+
+        //10.88s
+//        Sort.bubbleSort(data);
+        //1.647s
+//        Sort.selectionSort(data);
+        //0.612s
+//        Sort.insertionSort(data);
+        //6.074s
+        Sort.shellSort(data);
+
+        System.out.println((new Timestamp(System.currentTimeMillis()).getTime() - t1.getTime())/1000.0 + "s");
+
+    }
+
+    @Test
+    public void sortTest() {
+//        //int[] => list
+//        List list = Arrays.stream(Sort.bubbleSort(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1}))
+//                .boxed()
+//                .collect(Collectors.toList());
+//        System.out.println(Arrays.toString(Sort.bubbleSort(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1})));
+//        System.out.println(Arrays.toString(Sort.selectionSort(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1})));
+//        System.out.println(Arrays.toString(Sort.insertionSort(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1})));
+        System.out.println(Arrays.toString(Sort.shellSort(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1})));
     }
 }
